97468b396bf7e3d151a61d3bd6fb59618b73c6cc
Lune3 <113820374+Lune3@users.noreply.github.com>
2024-05-29 23:23:46 +0530

    implementing css

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
 import {currentWeather} from "./weatherAPI";
 
-currentWeather("Mumbai");
+currentWeather("London");
--- src/weatherAPI.js
+++ src/weatherAPI.js
@@ -3,14 +3,15 @@ const weather = (function () {
     async function getWeather(url){
         const response = await fetch(`${url}`,{mode: 'cors'});
         const countryWeather = await response.json();
-        console.log(countryWeather);
+        const currentWeather = countryWeather.current;
+        const forecast = countryWeather.forecast;
+        console.log(currentWeather,forecast);
     }
     return {getWeather};
 })();
 
 function currentWeather(country){
-    let url = `https://api.weatherapi.com/v1/current.json?key=7c1111326cb1491784f125003242905&q=`;
-    url = url+`${country}`;
+    let url = `https://api.weatherapi.com/v1/forecast.json?key=7c1111326cb1491784f125003242905&q=${country}&days=3`;
     weather.getWeather(url);
 }
 
